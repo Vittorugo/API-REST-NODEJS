26235913832d371e38159febaf0fe4a1d1c720b2
Victtor Hugo <hgloureirocosta@gmail.com>
2019-09-25 21:01:52 -0300

    teste post

--- Ntask/routes/index.js
+++ Ntask/routes/index.js
@@ -1,11 +1,29 @@
 const express    = require('express')
 const index = express.Router()
+const bodyParser = require('body-parser')
+
+const User = require('../models/Users')
 
 
-index.get('/', (req, res) => {
+index.post('/', async (req, res) => {
 
-    res.json({status: "NTask API "})
+    try {
 
+        const { name , email } = req.body
+
+        const user = await User.create({
+
+            name,
+            email
+
+        })
+
+        return res.status(200).send(user)
+
+    } catch (err) {
+
+        return res.status(400).send( { error: err})
+    }
 
 })
 
