ef5fc989a51256b6edb328f16a03c63afbb0a67c
Victtor Hugo <hgloureirocosta@gmail.com>
2019-09-25 21:03:43 -0300

    conexao com o banco mysql

--- Ntask/config/database.js
+++ Ntask/config/database.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
 
   username : 'root',
   password : 'root',
@@ -6,4 +6,20 @@ module.exports = {
   host: '127.0.0.1',
   dialect: 'mysql',
 
-}
+}*/
+
+const Sequelize  = require('sequelize')
+
+const connection = new Sequelize('ntask', 'root', 'root', {
+
+  host: '127.0.0.1',
+  dialect: 'mysql'
+
+})
+
+
+module.exports = connection
+
+
+
+ 
