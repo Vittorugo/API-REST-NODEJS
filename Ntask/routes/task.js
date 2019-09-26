276d53d74b1c69a285de052c1b4836fadcf65c83
Victtor Hugo <hgloureirocosta@gmail.com>
2019-09-25 23:15:41 -0300

    Começando a organizar rotas para as tarefas

--- Ntask/routes/task.js
+++ Ntask/routes/task.js
@@ -2,20 +2,68 @@ const express    = require('express')
 const task = express.Router()
 
 
-task.get('/task', (req, res ) => {
+const Task = require('../models/Tasks')
 
-    res.json({
 
-        tasks: [
+// Listando todos os dados Encontrados na tabela Tasks 
 
-            {title: "Fazer Compras"},
-            {title: "Consertar o PC"}
+task.get('/tasks', (req, res) => {
 
-        ]
+    Task.findAll({}).then( ( tasks ) => {
 
+        res.status(200).json({ msg: "Consulta Realizada com sucesso!", tasks})
+
+    }).catch( (err) => {
+
+
+        res.status(400).json({msg: "Erro ao consultar!"})
+    })
+
+
+})
+
+
+// Cadastrando novo valor na tabela Tasks
+
+task.post('/tasks', (req, res ) => {
+
+    /*
+    try {
+        
+        const { title , done } = req.body
+
+        let task = await Task.create({
+
+            title,
+            done
+        })
+
+        res.status(200).send(task)
+
+    } catch (error) {
+        res.status(400).send({err: error})        
+    }*/
+
+
+    Task.create({
+
+        title: req.body.title,
+        done: req.body.done
+
+    }).then( (tasks) => {
+
+
+        res.status(200).json({msg: "Tarefa cadastrada com sucesso!", tasks})
+    
+    }).catch( (err) => {
+
+
+        res.status(400).json({msg: "Erro ao cadastrar!", error: err})
     })
 
 })
+
+
 
 
 module.exports = task
