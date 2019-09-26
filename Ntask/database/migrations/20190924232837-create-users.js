8f766430ac5bae034b5979e6dcd29319a4563326
Victtor Hugo <hgloureirocosta@gmail.com>
2019-09-25 21:04:10 -0300

    editando o estilo do arquivo de migração

--- Ntask/database/migrations/20190924232837-create-users.js
+++ Ntask/database/migrations/20190924232837-create-users.js
@@ -1,9 +1,38 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    
+  up: (queryInterface, DataTypes) => {
+    return  queryInterface.createTable('Users', {
+      
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -11,9 +40,8 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-  },
 
-  down: (queryInterface, Sequelize) => {
+  down: (queryInterface) => {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
@@ -21,6 +49,6 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('Users');
   }
 };
