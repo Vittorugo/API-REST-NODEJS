8cc7639d9b525041169c4464b324698a34055264
Victtor Hugo <hgloureirocosta@gmail.com>
2019-09-25 21:03:12 -0300

    configurações para verificar se o banco esta sendo conectado

--- Ntask/index.js
+++ Ntask/index.js
@@ -3,7 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const index = require('./routes/index') 
 const task  = require('./routes/task')
-
+const Sequelize  = require('sequelize')
+const connection = require('./config/database')
+const User  = require('./models/Users')
+const Task  = require('./models/Tasks')
 
 // configurações ...
 
@@ -13,14 +16,35 @@ const task  = require('./routes/task')
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
 
+
+
+    // conexão com o banco 
+
+        
+
+        /*
+
+            Descomentar para verificar  se a conexao com o banco foi estabelica  
+        */
+        const connect = connection.authenticate().then( () => {
+
+            console.log('Conectado ao banco!')
+
+        }).catch( (err) => {
+
+            console.log('Erro ao conectar ao banco:' + err)
+
+        }).done()
+        
+    
+        //User.sync({force:true})
+        //Task.sync({force: true})
+
     //rotas 
 
         app.use(index)
         app.use(task)
     
-        // app.set('json spaces',4) //- melhorar a tabulação dos .json
-
-
 
     // conexao 
 
